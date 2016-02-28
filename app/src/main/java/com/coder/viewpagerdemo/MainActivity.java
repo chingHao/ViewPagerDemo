1942264b7f082f373fc838b48a576001c6b47362
coder <coder@coderdeMacBook-Pro.local>
2016-02-28 19:27:30 +0800

    Initial commit

---
+++ app/src/main/java/com/coder/viewpagerdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.coder.viewpagerdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    private ViewPager mViewPager;
+    private ImageView[] tips;
+    private ImageView[] mImageView;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
